eeab020aa66f67c1e04b8bd7d863099b514ab55d
Kamal Mango <kamal.mango7@gmail.com>
2022-02-20 11:38:01 +0100

    add Punklist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import CollectionCard from './components/CollectionCard';
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Punklist from './components/Punklist';
 
 function App() {
   const [punkListData, setPunkListData] = useState([])
@@ -23,12 +22,7 @@ function App() {
   return (
     <div className='app'>
       <Header />
-      <CollectionCard 
-        id={0} 
-        name={'Bandana Punk'} 
-        traits={[{'value': 7}]} 
-        image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg' 
-      />
+      <Punklist punkListData={punkListData} />
     </div>
   );
 }
