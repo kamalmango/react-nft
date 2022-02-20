95df8a3dfe07cccd4cbe264ce45a4f067c5ed754
Kamal Mango <kamal.mango7@gmail.com>
2022-02-20 11:17:51 +0100

    make opensea api call

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [punkListData, setPunkListData] = useState([])
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/assets?asset_contract_address=0x1679e5ECB16B758C4A45F1F1b1092267d5374ceb&order_direction=asc'
+      )
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
+
   return (
     <div className='app'>
       <Header />
