9cd7cea3a4f018497b379055210f6b7f17261872
Kamal Mango <kamal.mango7@gmail.com>
2022-02-21 19:30:12 +0100

    add key prop to collection cards

--- src/components/Punklist.js
+++ src/components/Punklist.js
@@ -6,9 +6,8 @@ const Punklist = ({ punkListData, setSelectedPunk }) => {
   return (
     <div className="punkList">
       {punkListData.map((punk) => (
-        <div onClick={() => setSelectedPunk(punk.token_id)}>
+        <div key={punk.token_id} onClick={() => setSelectedPunk(punk.token_id)}>
           <CollectionCard
-            key={punk.token_id}
             id={punk.token_id}
             name={punk.name}
             traits={punk.traits}
